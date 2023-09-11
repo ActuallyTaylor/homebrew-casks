fe646804d6d0e9475a8b446da09f791120e7ed1d
ActuallyTaylor <zachary.lineman@gmail.com>
2023-09-11 13:38:30 -0400

    Added open-ccan-monitor
    Signed-off-by: ActuallyTaylor <zachary.lineman@gmail.com>

--- open-can-monitor.rb
+++ open-can-monitor.rb
@@ -1,11 +1,11 @@
 cask "open-can-monitor" do
   version "0.9"
-  sha256 "40ad17d2106ec7c80bd2e7b0c71b62fc5edb8bcb2d262d2e198cab9f9621caa1"
+  sha256 :no_check
 
   url "https://github.com/ActuallyTaylor/OpenCANMonitor/releases/latest/download/OpenCANMonitor.dmg"
-  name "Open-CAN-Monitor"
-  desc "A feature rich CAN Monitoring application."
-  homepage "https://github.com/ActuallyTaylor/OpenCANMonitor"
+  name "open-can-monitor"
+  desc "CAN Network monitoring app with the ability to send and recieve CAN messages"
+  homepage "https://github.com/ActuallyTaylor/OpenCANMonitor/"
 
   app "OpenCANMonitor.app"
 end
